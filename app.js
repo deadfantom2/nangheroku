1bdb540e485eaa8151014cdd1de62df85bc9cc94
deadfantom2 <cameratest81@gmail.com>
2020-01-26 22:06:35 +0100

    1commi

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 
 // Middlewares
 app.use(express.json({ limit: "50mb" }));
-app.use(express.static(__dirname + "/dist/heroku/"));
+app.use(express.static(__dirname + "/dist/heroku"));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(logger("dev"));
 
