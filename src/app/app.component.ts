e1b16897828cc82d735b75049edeefd1e9c0be64
deadfantom2 <cameratest81@gmail.com>
2020-01-26 22:29:13 +0100

    1commi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { TitleService } from "./title.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-root",
@@ -7,8 +8,12 @@ import { TitleService } from "./title.service";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  constructor(private titleService: TitleService) {}
+  constructor(private titleService: TitleService, private router: Router) {}
   ngOnInit() {
     this.titleService.initTitle();
+  }
+
+  navigateToUsers() {
+    this.router.navigateByUrl("/users");
   }
 }
