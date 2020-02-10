2286c26c8d8a79f2ccf1b2149ed784b4aca3a96c
deadfantom2 <cameratest811@gmail.com>
2020-02-10 17:39:49 +0100

    qsd

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from '@angular/forms';
 import { PagesRoutingModule } from "./pages-routing.module";
 import { AllUsersComponent } from "./users/all-users/all-users.component";
 import { TestrouteComponent } from "./testroute/testroute.component";
@@ -16,6 +15,10 @@ import { AllOrdersComponent } from "./orders/all-orders/all-orders.component";
     TestrouteComponent
   ],
   exports: [],
-  imports: [PagesRoutingModule, CommonModule, RouterModule, FormsModule]
+  imports: [
+    PagesRoutingModule,
+    CommonModule,
+    RouterModule
+  ]
 })
 export class PagesModule { }
