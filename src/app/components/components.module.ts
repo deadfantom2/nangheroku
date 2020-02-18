a85c6a3cfc3513f7ace476d1351c3312a16ffde9
deadfantom2 <cameratest81@gmail.com>
2020-02-18 22:28:05 +0100

    d

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,22 +3,30 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { ComponentsRoutingModule } from "./components-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
 
 // Components
 import { ComponentsComponent } from "./components.component";
 import { AllUsersComponent } from "./users/all-users/all-users.component";
 import { AllOrdersComponent } from "./orders/all-orders/all-orders.component";
 import { TestrouteComponent } from "./testroute/testroute.component";
-import { UserPageComponent } from '../pages'
+import { UserPageComponent } from "../pages";
 
 @NgModule({
   declarations: [
     ComponentsComponent,
     AllUsersComponent,
     AllOrdersComponent,
-    TestrouteComponent, UserPageComponent
+    TestrouteComponent,
+    UserPageComponent
   ],
   exports: [],
-  imports: [ComponentsRoutingModule, CommonModule, RouterModule]
+  imports: [
+    ComponentsRoutingModule,
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule
+    // FormsModule
+  ]
 })
-export class PagesModule { }
+export class PagesModule {}
--- src/app/components/users/all-users/all-users.component.ts
+++ src/app/components/users/all-users/all-users.component.ts
@@ -3,6 +3,7 @@ import { Observable, pipe, Subscription } from "rxjs";
 import { User } from "../../../_models/user";
 import { UsersService } from "../../../_services";
 import { map, catchError, tap } from "rxjs/operators";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-all-users",
@@ -10,17 +11,33 @@ import { map, catchError, tap } from "rxjs/operators";
   styleUrls: ["./all-users.component.scss"]
 })
 export class AllUsersComponent implements OnInit {
-
   // Async Observable data stream
   users$: Observable<User[]>;
 
   public allUsersNormal: User[];
   public allUsersAsync$: Observable<User[]>;
 
-  constructor(private _usersService: UsersService) { }
+  public form: FormGroup;
+  public aSub: Subscription;
+
+  constructor(private _usersService: UsersService) {}
 
   ngOnInit() {
     this.getAllUsers();
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      password_confirme: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8)
+      ])
+    });
   }
 
   /** GET ALL USERS */
@@ -30,15 +47,18 @@ export class AllUsersComponent implements OnInit {
     // return this._usersService.getAll();  // 2 method by protected route
   }
 
+  /** CREATE ONE USER  */
+  public createUser(user: User) {
+    console.log(this.form.value);
+  }
+
   /** DELETE USER BY ID */
   public deleteUser(user: User) {
     this._usersService.deleteUser(user);
   }
 
-
   /** SORTING */
   public doSortName() {
     this._usersService.sortObject();
   }
-
 }
